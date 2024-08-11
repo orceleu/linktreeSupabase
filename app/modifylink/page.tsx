da8673b0679384ed4a324e7b22e7dda60867a69a
orcel <orceleu@gmail.com>
2024-08-11 13:34:23 -0400

    reload4

--- app/modifylink/page.tsx
+++ app/modifylink/page.tsx
@@ -1,25 +1,25 @@
 "use client";
 import React from "react";
-import { useSearchParams } from "next/navigation";
-import { Suspense } from "react";
+//import { useSearchParams } from "next/navigation";
+//import { Suspense } from "react";
 
 function Search() {
-  const searchParams = useSearchParams();
-  console.log(searchParams.get("search"));
-  const AAAA = searchParams.get("search");
-  const BBBB = searchParams.get("selectedurl");
+  // const searchParams = useSearchParams();
+  // console.log(searchParams.get("search"));
+  // const AAAA = searchParams.get("search");
+  //const BBBB = searchParams.get("selectedurl");
   return (
     <div>
-      <p>{AAAA}</p>
-      <p>{BBBB}</p>
+      <p>prob</p>
     </div>
   );
 }
 
-export default function Modifylink() {
+/*export default function Modifylink() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <Search />
     </Suspense>
   );
 }
+*/
