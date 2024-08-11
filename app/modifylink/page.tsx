0f18bc5b1e79ecca180e2ad251f03687f0afbe01
orcel <orceleu@gmail.com>
2024-08-11 13:21:33 -0400

    reload

--- app/modifylink/page.tsx
+++ app/modifylink/page.tsx
@@ -1,15 +1,18 @@
 "use client";
 import React from "react";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 export default function Modifylink() {
   const searchParams = useSearchParams();
   console.log(searchParams.get("search"));
   const AAAA = searchParams.get("search");
   const BBBB = searchParams.get("selectedurl");
   return (
-    <div>
-      <p>{AAAA}</p>
-      <p>{BBBB}</p>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <p>{AAAA}</p>
+        <p>{BBBB}</p>
+      </div>{" "}
+    </Suspense>
   );
 }
